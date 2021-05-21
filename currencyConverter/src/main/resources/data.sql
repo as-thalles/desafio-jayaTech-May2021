fc4ef58d69183873fe466a4e2748c5780bc4476e
Thalles A.S <thalles.al.slv@gmail.com>
2021-05-20 22:26:38 -0300

    User model, database seed.

---
+++ currencyConverter/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(name) VALUES ('Christine Daae');
+INSERT INTO users(name) VALUES ('Lisbeth Salander');
+INSERT INTO users(name) VALUES ('Gregory House');
+INSERT INTO users(name) VALUES ('Will Graham');
+INSERT INTO users(name) VALUES ('Clarice Starling');
